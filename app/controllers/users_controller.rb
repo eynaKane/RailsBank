abdaa61ce8d6f6f7bdd3ca6cdfe3ef9827f95289
ekane <ekane@optoro.com>
2016-08-24 10:51:32 -0400

    Cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :require_user, only: [:show]
+  before_action :require_user, only: [:edit, :update, :show]
 
   def new
     @user = User.new
